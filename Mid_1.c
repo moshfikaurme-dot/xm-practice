6c0e091d9685bdcf6594214d6e6ced99a315763d
Moshfika Najnin <moshfikaurme@gmail.com>
2025-12-08 21:39:17 +0600

    feat: add basic info printing program (name, DOB, mobile)

---
+++ Mid_1.c
@@ -0,0 +1,11 @@
+/*write a C program to print your name.
+date of brith. and mobile number.
+*/
+
+#include<stdio.h>
+int main(){
+    printf("name: moshfika najnin urme\n");
+    printf("date of brith: 28 feb 2003\n");
+    printf("mobile number: 01312865535");
+    return 0;
+    }
