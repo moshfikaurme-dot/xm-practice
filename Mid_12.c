1702472396ac69be4955a2a057401759acccf14d
Moshfika Najnin <moshfikaurme@gmail.com>
2025-12-20 18:05:10 +0600

    Add program to print even numbers up to 50 using a for loop

---
+++ Mid_12.c
@@ -0,0 +1,17 @@
+/*  Write a C program that prints all even numbers between 1 and 50 (inclusive)..*/
+
+#include <stdio.h>
+
+int main() {
+
+int number[50];
+
+for (int i=0; i<=50; i++)
+        {
+    if(i%2 ==0)
+        printf("%d\n",i);
+
+
+            }
+
+}
